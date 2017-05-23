1d9220a7c41f80029597fb9f1e6bcfe1ba8d1a06
Felipe Lira <felipelira3@yahoo.com.br>
2017-05-24 00:04:45 +0200

    Update genes_per_contigs.py

--- genes_per_contigs.py
+++ genes_per_contigs.py
@@ -14,18 +14,6 @@ file = open(sys.argv[1], "r")
 dict = {}
 
 for sequence in file:
-#	name = sequence.id
-#	contig = name.split('_')[0]+'_'+name.split('_')[1]
-#	direction = sequence.id.split('#')[3]
-#	if contig not in dict:
-#		dict[contig] = [name]
-#	else:
-#		dict[contig].append(name)
-
-#for k, v in dict.iteritems():
-#	print k+'\t'+ str(len(v))
-#	if len(v) >= 2:
-#	print k+'\t'+ str('\t'.join(v))
 	if sequence.startswith(">"):
 		name = sequence.strip().replace(">", "").split(" # ")
 		contig = name[0].strip().split("_")[0]+"_"+name[0].strip().split("_")[1]+"_"+name[0].strip
