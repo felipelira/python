e30b212cc436640aad18c70bfdb1461e74a79b00
Felipe Lira <felipelira3@yahoo.com.br>
2017-06-15 00:21:39 +0200

    Create frequency.py

--- frequency.py
+++ frequency.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# created by Felipe Lira Feb/2017
-# to calculate frequency from BLAST tabular results
-# based on subject column
-# useful to calculate the frequency for any 
-# table with two or more columns.
+# Felipe Lira 2017
+# calculate frequency from BLAST tabular results
+# It uses the bhit.file
+
+# frequency.py bhit.file > output.file
 
 import sys
 from collections import Counter
@@ -11,11 +11,12 @@ from collections import Counter
 file = open(sys.argv[1])
 
 list = []
+dict = {}
+
 for line in file:
-        name = line.strip().split("\t")[1]
-        name_correct = name.split("|")[0] + "|" + name.split("|")[1] + "|" +  name.split("|")[2] + "|" +  name.split("|")[3]+"|"
-        list.append(name_correct)
+        gi = line.strip().split("\t")[1]
+        name = line.strip().split("\t")[2]
+        list.append(gi)
 
 for i in Counter(list).most_common():
         print i[0]+"\t"+ str(i[1])
-
