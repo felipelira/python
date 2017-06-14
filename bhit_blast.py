91a69219d7a1662768a4068f3bd957838e799aa9
Felipe Lira <felipelira3@yahoo.com.br>
2017-06-15 00:23:30 +0200

    Create bhit_blast.py

--- bhit_blast.py
+++ bhit_blast.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# Felipe Lira 2017
+# Felipe Lira 201
 # Retrieve the best hit from a BLAST tabular result
-# usage: bhit_blast.py BLAST.m8 > output.m8.bhit
+# usage: bhit_blast.py BLAST_m8.tab > output.m8
 
 import sys
 from collections import Counter
@@ -10,10 +10,10 @@ file = open(sys.argv[1], "ru")
 d = {}
 
 for line in file:
-	query = line.strip().split('\t')[0]
-	data = line.strip().split('\t')[1:]
-	if query not in d:
-		d[query]= data
+        query = line.strip().split('\t')[0]
+        data = line.strip().split('\t')[1:]
+        if query not in d:
+                d[query]= data
 
 for k, v in d.iteritems():
         print k+'\t'+ str('\t'.join(v))
